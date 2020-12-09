1f34d12cee2c98a11b8beb905b2a376209eca016
jaydxn1 <NIL>
2020-12-09 18:58:07 +0800

    added security features

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ from schedules import schedules, unregister
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 import logging
+from dotenv import load_dotenv
+import os
 
 #Logging Module
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 #Token
-updater = Updater(token='1395949263:AAECQtjOsNWQUeIyHL02JhmIyoGNRw1Txt8', use_context=True)
+updater = Updater(token=os.getenv('SMUX_BOT_TOKEN'), use_context=True)
 dispatcher = updater.dispatcher
 
 start_handler = CommandHandler('start', start)
